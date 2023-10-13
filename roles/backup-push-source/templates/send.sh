8ffb3a74baee4b154aa0bbdb5f053412d5172260
Julie Koubova <julie@koubova.net>
2023-10-13 20:32:36 +0200

    backup

--- roles/backup-push-source/templates/send.sh
+++ roles/backup-push-source/templates/send.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+if [ `whoami` != "{{ backup_user }}" ]; then
+  exec su -s /bin/sh "{{ backup_user }}" "{{ backup_home }}/send.sh"
+fi
+
 {% for target in backup_targets | dict2items %}
 {% if target.key != 'local' %}
 flock --nonblock '{{ backup_home }}/send.{{ target.key }}.lock' zfs-autobackup \
+  --filter-properties mountpoint,refreservation \
+  --set-properties readonly=on \
+  --force \
   --no-snapshot \
-  --rate 50M \
   --ssh-target '{{ target.key }}' \
   --zfs-compressed \
   {{ backup_job_common_args }} \
--- roles/backup-push-source/templates/snapshot.sh
+++ roles/backup-push-source/templates/snapshot.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if [ `whoami` != "{{ backup_user }}" ]; then
+  exec su -s /bin/sh "{{ backup_user }}" "{{ backup_home }}/snapshot.sh"
+fi
+
 {% for target in backup_targets | dict2items %}
 flock --nonblock '{{ backup_home }}/snapshot.{{ target.key }}.lock' \
   zfs-autobackup \
