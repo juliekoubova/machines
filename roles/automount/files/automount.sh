5f0d55a7a264467041dddb223feec16292375f4a
Julie Koubova <julie@koubova.net>
2023-12-03 01:19:03 +0100

    automount

---
+++ roles/automount/files/automount.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+: "${AUTOMOUNT_ROOT:=/media}"
+: "${AUTOMOUNT_OPTS:=sync,noatime,noexec,nosuid,nodev}"
+: "${AUTOMOUNT_GROUP:=plugdev}"
+
+action="${1}"
+device_name="${2}"
+device_path="/dev/${device_name}"
+mountpoint="${AUTOMOUNT_ROOT}/${device_name}"
+
+log() {
+  logger -s -t "automount.sh ${device_name}" "$@"
+}
+
+fail() {
+  log -p user.error "${1}"
+  exit 1
+}
+
+if [ "${action}" == "remove" ]; then
+  if mountpoint -q "${mountpoint}"; then
+    log "Unmounting ${mountpoint}"
+    umount "${mountpoint}" || fail "Failed to unmount"
+  fi
+  exit 0
+fi
+
+[ -n "${device_name}" ] || fail "Device name not specified"
+[ -b "${device_path}" ] || fail "Not a block device"
+
+fstype=`lsblk "${device_path}" -no fstype` || \
+  fail "Could not determine filesystem type"
+
+gid=`getent group "${AUTOMOUNT_GROUP}" | cut -d: -f3`
+[ -n "${gid}" ] || fail "Could not determine group id of '${AUTOMOUNT_GROUP}'"
+
+if [ ! -d "${mountpoint}" ]; then
+  mkdir "${AUTOMOUNT_ROOT}/${device_name}" || \
+    fail "Could not create ${AUTOMOUNT_ROOT}/${device_name}"
+fi
+
+case "${fstype}" in
+  vfat)
+    log "Mounting as vfat"
+    mount -t vfat -o "${AUTOMOUNT_OPTS},dmask=002,fmask=113,gid=${gid}" "${device_path}" "${mountpoint}" \
+      || fail "Failed to mount"
+    exit 0
+    ;;
+  *) fail "Unknown fstype: '${fstype}'"
+esac
